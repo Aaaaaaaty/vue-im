7ea5f94abcdbde50f3e9b5a8987e6ad514c1ab1b
幕希Lin <2103887953@qq.com>
2016-09-30 13:22:38 +0800

    router active

--- src/components/new.vue
+++ src/components/new.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>new}</h1>
+  <h1>new</h1>
 </template>
 
 <script>
--- src/components/user.vue
+++ src/components/user.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="nav"></div>
-  <div class="footer"></div>
+  <div id="nav">{{viewName}}</div>
+  <ul id="footer">
+    <li v-bind:class="footerNav" id="alivelist" @click="link">
+      <i class="iconfont" >&#xe600</i>biu
+    </li>
+    <li v-bind:class="footerNav" id="userlit" @click="link">
+      <i class="iconfont" >&#xe603</i>通讯录
+    </li>
+    <li v-bind:class="footerNav" id="find" @click="link">
+      <i class="iconfont" id="xe602">&#xe602</i>发现
+    </li>
+    <li v-bind:class="footerNav" id="account" @click="link">
+      <i class="iconfont" >&#xe601</i>我
+    </li>
+  </ul>
+
   <router-view></router-view>
 </template>
 
@@ -8,13 +22,74 @@
   export default {
     data: function() {
       return {
-        viewName: 'biubiu'
+        viewName: 'biubiu',
+        footerNav: 'footer-nav',
+        isActive: false,
+        active: true
       }
+    },
+    methods: {
+      link: function(e) {
+        switch(e.currentTarget.id)
+        {
+          case 'alivelist':
+            this.$router.go('/user/')
+            break
+          case 'userlit':
+            console.log(e.currentTarget.id)
+            this.$router.go('/user/userlist')
+            break
+          case 'find':
+            this.$router.go('/user/new')
+            break
+          case 'account':
+            this.$router.go('/user/account')
+        }
+        
+      }
+    },
+    created: function() {
+      console.log(document.location)
     }
   }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style lang="scss">
+  @import '../utils/utils.scss';
+  #nav {
+    text-align: center;
+  }
+  #footer {
+    position: fixed;
+    bottom: 0px;
+    width: 100%;
+    background-color: $gray-bg;
+    .footer-nav {
+      display: block;
+      position: relative;
+      width: 25%;
+      padding-top: 30px;
+      color: $gray-holder;
+      text-align: center;
+      font-size: 12px;
+      .iconfont {
+        font-family: "iconfont";
+        font-size: 24px;
+        font-style: normal;
+        position: absolute;
+        left: 38%;
+        top: 7%;
+      }
+      #xe602 {
+        font-size: 20px;
+        left: 39%;
+        top: 12%;
+      }
+      float: left;
+      list-style: none;
+    }
+    .active {
+      color: $blue-icon;
+    }
+  }
 </style>
