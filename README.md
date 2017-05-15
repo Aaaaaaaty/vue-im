5b3aee243c809311bdc8e82cf0690e8551f552fc
An <2103887953@qq.com>
2017-05-15 16:12:27 +0800

    delete client some code

--- README.md
+++ README.md
@@ -1,37 +1,32 @@
-# vue-im
-
-> A Vue.js project
-
-## Build Setup
-
-``` bash
-# install dependencies
-npm install
-
-# serve with hot reload at localhost:8080
-npm run dev
-
-# build for production with minification
-npm run build
-
-# run unit tests
-npm run unit
-
-# run e2e tests
-npm run e2e
-
-# run all tests
-npm test
+## Download
 ```
+git clone https://github.com/Aaaaaaaty/vue-im
+```
+## Deploy
+#### Client
+```
+npm install
+```
+#### Server
+```
+npm install -g nodemon //[node's self-restart tool](https://nodemon.io/)
+```
+#### Database
+```
+brew install mongodb
 
-For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
-
-仿照微信的基于vue, node, express, monogo, socket.io的即时通讯应用
-功能：
-	私聊
-	群聊
-	通讯录
-	聊天记录
-	在网页适配好后用electron进行打包成桌面应用
-安装 MongoDB：brew install mongodb
-运行 mongodb: sudo mongod --dbpath=/data/db --port=27017
+```
+## Run
+### Client
+```
+npm run dev
+```
+### Server
+```
+nodemon ./server/bin/www
+```
+### Database
+```
+sudo mongod --dbpath=/data/db --port=27017
+```
+### Features
--- src/client.js
+++ src/client.js
@@ -3,13 +3,8 @@ import settings from './settings.js'
 const CHAT={
   msgObj:document.getElementsByClassName("body-wrapper")[0],
   username:null,
-  userid:null,
-  color:null,
   socket:null,
-  onlineCount:0,
-  onlineUsers:null,
   msgArr:[],
-  lock:{},
   //让浏览器滚动条保持在最低部
   scrollToBottom:function(){
     window.scrollTo(0, 900000);
@@ -23,57 +18,12 @@ const CHAT={
   submit:function(obj){
     this.socket.emit('sendMsg', obj);
   },
-  genUid:function(){
-    return new Date().getTime()+""+Math.floor(Math.random()*899+100);
-  },
-  //更新系统消息，本例中在用户加入、退出的时候调用
-  updateSysMsg:function(o, action){
-    //当前在线用户列表
-    this.onlineUsers=o.onlineUsers;
-    //当前在线人数
-    this.onlineCount = o.onlineCount;
-    //新加入用户的信息
-    var user = o.user;
-      
-    //更新在线人数
-    var userhtml = '';
-    var separator = '';
-    // for(key in onlineUsers) {
-   //        if(onlineUsers.hasOwnProperty(key)){
-    //    userhtml += separator+onlineUsers[key];
-    //    separator = '、';
-    //  }
-   //  }
-    // d.getElementById("onlinecount").innerHTML = '当前共有 '+onlineCount+' 人在线，在线列表：'+userhtml;
-    
-    // //添加系统消息
-    // var html = '';
-    // html += '<div class="msg-system">';
-    // html += user.username;
-    // html += (action == 'login') ? ' 加入了聊天室' : ' 退出了聊天室';
-    // html += '</div>';
-    // var section = d.createElement('section');
-    // section.className = 'system J-mjrlinkWrap J-cutMsg';
-    // section.innerHTML = html;
-    // this.msgObj.appendChild(section);  
-    // this.scrollToBottom();
-  },
-  changeInfo(){
-    this.userid = localStorage.getItem('userid');
-    this.username = localStorage.getItem('name');
-    this.color = localStorage.getItem('color');
-    this.weichat = localStorage.getItem('weichat');
-    this.socket.emit('changeInfo', {userid:this.userid, username:this.username,color:this.color,weichat:this.weichat});
-  },
-  genUid:function(){
-    return new Date().getTime()+""+Math.floor(Math.random()*899+100);
-  },
   message: function(username) {
     console.log('message')
       this.socket.on('to' + username, function(obj) {
         CHAT.msgArr.push(obj)
         console.log('CHAT.msgArr', obj)
-      })    
+      })
   },
   init:function(username){
     //连接websocket后端服务器
@@ -81,36 +31,8 @@ const CHAT={
     this.socket.on('open', function() {
       console.log('已连接')
     })
-    // console.log(toUser, fromUser)
     this.socket.emit('addUser', username)
-    //告诉服务器端有用户登录
-    // this.socket.emit('login', {userid:this.userid, username:this.username,color:this.color,weichat:this.weichat});
-    //心跳包，30s左右无数据浏览器会断开连接Heartbeat
-
-    // setInterval(() => {
-    //   this.socket.emit('heartbeat', 1);
-    // },10000)
-    //监听新用户登录
-    // this.socket.on('login', function(obj){
-    //   CHAT.updateSysMsg(obj, 'logout');
-    //   CHAT.msgArr.push(obj) 
-    // });
-    
-    // this.socket.on('changeInfo', function(o){
-    //   CHAT.onlineUsers[o.userid]=o
-    //   console.log(o)  
-    // });
-    //监听用户退出
-    // this.socket.on('logout', function(o){
-    //   CHAT.updateSysMsg(o, 'logout');
-    // });
-    
-    //监听消息发送
-    // this.socket.on('message', function(obj){
-      // var isme = (obj.userid == CHAT.userid) ? true : false;
-    //   CHAT.msgArr.push(obj) 
-    // });
 
   }
-} 
+}
 export default CHAT
